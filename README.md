77aea513cf370498c161735bd0b7546946b8d771
210083363 <210083363@aston.ac.uk>
2022-11-23 18:41:57 +0000

    for everything holy please work

---
+++ README.md
@@ -0,0 +1,28 @@
+# How to
+
+`php artisan serve` <br>
+`npm run dev`
+
+xampp apache & mysql
+
+needed before running <br>
+https://nodejs.org/en/download/
+
+First time setup <br>
+`php artisan migrate`
+
+Main files <br>
+```
+STANCE  
+├── app  
+│   └── Http
+│       ├── Controllers
+│       │   └── AuthController.php  
+│       └── Middlware
+│           └── AuthCheck.php
+├── resources
+│   └── views
+│       └── *.php
+└── routes
+    └── web.php
+```
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('home');
+});
+
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'register']);
+Route::post('/register-user', [AuthController::class, 'registerUser'])->name('register-user');
+Route::post('/login-user', [AuthController::class, 'loginUser'])->name('login-user');
+
+Route::get('/home', [AuthController::class, 'home'])->middleware('isLoggedIn');
+Route::get('/logout', [AuthController::class, 'logout']);
